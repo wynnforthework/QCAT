f23f50246876eb64e9ede6c54578b663ac93d150
Wynn <wynnforthework@gmail.com>
2025-08-16 02:56:03 +0800

    提交一些前端报错检查配置文件

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  pluginReact.configs.flat.recommended,
+]);
